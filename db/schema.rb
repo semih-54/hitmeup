539d0f8856eea6ed965cb8b2411ceb862aef75b4
Semih cakmak <cakmak.semih@hotmail.com>
2023-11-22 13:30:07 +0800

    seeds migration contract and relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_042953) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_042232) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_042953) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "victim_name"
+    t.integer "victim_age"
+    t.text "victim_location"
+    t.text "victim_description"
+    t.text "budget"
+    t.string "status"
     t.index ["scenario_id"], name: "index_contracts_on_scenario_id"
     t.index ["user_id"], name: "index_contracts_on_user_id"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "date"
+
+# ApplicationRecord.destroy_all
+# Contract.destroy_all
+# HitmanScenario.destroy_all
+# HitmanSkill.destroy_all
+# Hitman.destroy_all
+# Scenario.destroy_all
+# Skill.destroy_all
+# User.destroy_all
+
+# Contract.create!([{
+#   expiration_date: Date.new(2024,01,23),
+#   victim_name: "Ronaldinho",
+#   victim_age: 43,
+#   victim_location: "Rio Verde, Brasil",
+#   victim_description: "A famous football player famous for his moves and exceptional style. He is very well known so it will not be an easy task but I want him gone since he scored the 1-0 with Brasil against Turkey in 2002. A shame that this should be his last FlipFlap.",
+#   budget: "Kidney (left) and 2L Virgin Olive Oil from Turkey",
+#   status: "accepted"
+# },
+# {
+#   expiration_date: Date.new(2023,12,24),
+#   victim_name: "Brendan",
+#   victim_age: 34,
+#   victim_location: "Canggu, Bali, Indonesia",
+#   victim_description: "Brendan's my neighbour and thinks 'innit?' or 'bevvy' are acceptable words and Alcohol is his only source of fluid intake. Although this should be a red line and reason enough ... he also thinks it's okay to put up a christmas tree and sing christmas songs in Bali at 32°. So I want him to have an 'accident' while decorating his tree on the 24. December.",
+#   budget: "2x Nasi Campur and Brendan's Bike",
+#   status: "pending"
+# },
+# {
+#   expiration_date: Date.new(2026,05,12),
+#   victim_name: "",
+#   victim_age: 43,
+#   victim_location: "Rio Verde, Brasil",
+#   victim_description: "",
+#   budget: "Kidney (left) and 2L Virgin Olive Oil",
+#   status: "pending"
+# }])
+
+# p "Created #{Contract.count} contracts"
