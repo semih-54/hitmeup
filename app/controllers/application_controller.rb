f1105560e0b1afeb7892c6a20fd62bc0ce3c747c
Pierre Cahuzac <pierre.cahuzac1@gmail.com>
2023-11-22 15:39:49 +0800

    routes hitman

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    user_contracts_path(current_user)
+  def after_sign_up_path_for(resource)
+    if current_user.hitman?
+      new_hitman_path
+    else
+      contracts_path(current_user)
+    end
   end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :hitman])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :hitman])
   end
 end
--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,16 +1,20 @@
 class ContractsController < ApplicationController
   def index
-    @contracts = Contract.all
+    @contracts = Contract.where(user: current_user)
   end
 
   def new
     @contract = Contract.new
   end
 
+  def my_contracts
+    @contracts = Contract.where(hitman: current_user.hitman)
+  end
+
   def create
     @contract = Contract.new
     if @contract.save
-      redirect_to user_contracts_path
+      redirect_to contracts_path
     else
       render :new, status: :unprocessable_entity
     end
